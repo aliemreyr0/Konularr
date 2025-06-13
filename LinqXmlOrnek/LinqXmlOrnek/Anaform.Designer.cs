9ebeb34071be954dc5ee8f137a8e002f21c345ca
aliemreyr0 <yaraliemre0@gmail.com>
2025-06-13 20:24:10 +0300

    Add files via upload

---
+++ LinqXmlOrnek/LinqXmlOrnek/Anaform.Designer.cs
@@ -0,0 +1,244 @@
+﻿
+namespace LinqXmlOrnek
+{
+    partial class Anaform
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            textBox1 = new System.Windows.Forms.TextBox();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            button8 = new System.Windows.Forms.Button();
+            button9 = new System.Windows.Forms.Button();
+            button10 = new System.Windows.Forms.Button();
+            button11 = new System.Windows.Forms.Button();
+            button12 = new System.Windows.Forms.Button();
+            button13 = new System.Windows.Forms.Button();
+            button14 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(89, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Xml Listele 1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(12, 75);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            textBox1.Size = new System.Drawing.Size(744, 363);
+            textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(107, 12);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(75, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Xml Listele 2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(188, 12);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(101, 23);
+            button3.TabIndex = 3;
+            button3.Text = "İsim Elementi";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(295, 12);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(96, 23);
+            button4.TabIndex = 4;
+            button4.Text = "Erkek İşçiler";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new System.Drawing.Point(403, 12);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(99, 23);
+            button5.TabIndex = 5;
+            button5.Text = "Kadın İşçiler";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new System.Drawing.Point(508, 12);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(125, 23);
+            button6.TabIndex = 6;
+            button6.Text = "Ev Telefonu";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new System.Drawing.Point(639, 12);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(117, 23);
+            button7.TabIndex = 7;
+            button7.Text = "İş Telefonu";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new System.Drawing.Point(12, 46);
+            button8.Name = "button8";
+            button8.Size = new System.Drawing.Size(89, 23);
+            button8.TabIndex = 8;
+            button8.Text = "Kocaeli Şehri";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new System.Drawing.Point(107, 46);
+            button9.Name = "button9";
+            button9.Size = new System.Drawing.Size(75, 23);
+            button9.TabIndex = 9;
+            button9.Text = "İsimler";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new System.Drawing.Point(188, 46);
+            button10.Name = "button10";
+            button10.Size = new System.Drawing.Size(101, 23);
+            button10.TabIndex = 10;
+            button10.Text = "Xml Yazdır";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new System.Drawing.Point(295, 46);
+            button11.Name = "button11";
+            button11.Size = new System.Drawing.Size(96, 23);
+            button11.TabIndex = 11;
+            button11.Text = "Xml Oku";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new System.Drawing.Point(403, 46);
+            button12.Name = "button12";
+            button12.Size = new System.Drawing.Size(98, 23);
+            button12.TabIndex = 12;
+            button12.Text = "Kitapları Oku";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new System.Drawing.Point(508, 46);
+            button13.Name = "button13";
+            button13.Size = new System.Drawing.Size(125, 23);
+            button13.TabIndex = 13;
+            button13.Text = "Öğrenciler";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new System.Drawing.Point(639, 46);
+            button14.Name = "button14";
+            button14.Size = new System.Drawing.Size(117, 23);
+            button14.TabIndex = 14;
+            button14.Text = "Yaşı 20-25 Arası";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // Anaform
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(769, 458);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Anaform";
+            Text = "Xml";
+            Load += Anaform_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+    }
+}
+
---
+++ LinqXmlOrnek/LinqXmlOrnek/Anaform.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO;
+/*
+ Mustafa OF
+ Kocaeli Myo
+ */
+namespace LinqXmlOrnek
+{
+    public partial class Anaform : Form
+    {
+        //Xml Dosyasını okuma
+        XElement xmldosya = XElement.Load("isciler.xml");//İnternet adresi olabilir
+
+        public Anaform()
+        {
+            InitializeComponent();
+        }
+
+        private void Anaform_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Düğümleri Xml olarak alma
+            textBox1.Text = "";
+            IEnumerable<XElement> altdugumler = xmldosya.Elements();
+            //Listeleyelim
+            foreach (var eleman in altdugumler)
+                textBox1.Text += eleman;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Listeleme
+            textBox1.Text = "";
+            XDocument xmldosya = XDocument.Load("isciler.xml");//En üstteki (root) düğüm gösterilir.
+            IEnumerable<XElement> altdugumler = xmldosya.Elements();
+            //Düğümleri listeler
+            foreach (var eleman in altdugumler)
+                textBox1.Text += eleman;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Belirli bir element'i elde etme
+            textBox1.Text = "";
+            IEnumerable<XElement> altdugumler = xmldosya.Elements();
+            //Listeleme
+            foreach (var eleman in altdugumler)
+            {
+                textBox1.Text += string.Format("{0} adlı personelin kimliği {1} ve cinsiyeti {2}",
+                    eleman.Element("isim").Value,
+                    eleman.Element("kimlik").Value,
+                    eleman.Element("cinsiyet").Value +
+                    eleman.Element("telefon").Value + //Düğüm isimleri xml dosyasındaki gibi olmalıdır.
+                    "\r\n");
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Erkek işçiler
+            textBox1.Text = "";
+            //Linq
+            var erkekisciler = from deg1 in xmldosya.Elements("isci")
+                               where deg1.Element("cinsiyet").Value == "Erkek"
+                               select deg1;
+            //Listeleme
+            foreach (XElement eleman in erkekisciler)
+                textBox1.Text += eleman;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Kadın işçiler
+            textBox1.Text = "";
+            //Linq
+            var kadinisciler = from deg1 in xmldosya.Elements("isci")
+                               where deg1.Element("cinsiyet").Value == "Kadın"
+                               select deg1;
+            //Listeleme
+            foreach (XElement eleman in kadinisciler)
+                textBox1.Text += eleman.Element("isim").Value + " Telefon:" + eleman.Element("telefon").Value;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Ev Telefonu olanlar
+            textBox1.Text = "";
+            //Linq
+            var evtelefonu = from deg1 in xmldosya.Elements("isci")
+                             where (string)deg1.Element("telefon").Attribute("tip") == "ev"
+                             select deg1;
+
+            //Listeleme
+            foreach (XElement eleman in evtelefonu)
+                textBox1.Text += eleman.Element("isim").Value + " Telefon:" + eleman.Element("telefon").Value;
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //İş Telefonu olanlar
+            textBox1.Text = "";
+            //Linq
+            var istelefonu = from deg1 in xmldosya.Elements("isci")
+                             where (string)deg1.Element("telefon").Attribute("tip") == "iş"
+                             select deg1;
+
+            //Listeleme
+            foreach (XElement eleman in istelefonu)
+                textBox1.Text += eleman.Element("isim").Value + " Telefon:" + eleman.Element("telefon").Value + "\r\n";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Kocaeli şehri
+            textBox1.Text = "";
+            //Linq
+            var sehir = from deg1 in xmldosya.Elements("isci")
+                        where (string)deg1.Element("adres").Element("sehir").Value == "Kocaeli"
+                        select deg1;
+            //Listeleme
+            foreach (XElement eleman in sehir)
+                textBox1.Text += eleman.Element("isim").Value
+                    + " Şehir:" + eleman.Element("adres").Element("sehir").Value +
+                    " Cadde:" + eleman.Element("adres").Element("cadde").Value +
+                    " Posta Kodu:" + eleman.Element("adres").Element("postakodu").Value +
+                    " Ülke:" + eleman.Element("adres").Element("ulke").Value +
+                    "\r\n";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //İsimleri döndürelim
+            textBox1.Text = "";
+            //Descendants metodu ile sadece verilen düğüm ismindeki değerler elde edilebilir.
+            foreach (XElement eleman in xmldosya.Descendants("isim"))
+            {
+                textBox1.Text += (string)eleman + "\r\n";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //Xml dosyası yazdırma            
+            //Xml düğümlerinin/elementlerinin oluşturulması
+            XDocument xmldosyabilgi = new XDocument(
+                new XDeclaration("1.0", "utf-8", null),
+                new XElement("Personel",
+                new XComment("Personel bilgileri oluşturuldu"),
+                new XElement("kimlik", 5),
+                new XElement("isim", "Ahmet Eren"),
+                new XElement("birimi", "Muhasebe"),
+                new XElement("cinsiyet", "Erkek"))
+                );
+            //Dosya yazma işlemleri
+            StringWriter metinyaz = new StringWriter();
+            xmldosyabilgi.Save(metinyaz);
+            //Sonucu listele
+            textBox1.Text = metinyaz.ToString();
+            //Dosyayı oluşturalım
+            StreamWriter dosyayaz = new StreamWriter("personelbilgi.xml");
+            dosyayaz.Write(metinyaz.ToString());
+            dosyayaz.Close();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //XmlReader nesnesi ile xml verisini okuma
+            textBox1.Text = "";
+            XmlReader xmloku = XmlReader.Create("isciler.xml");
+            XElement elementler = XElement.Load(xmloku);
+            textBox1.Text = elementler.ToString();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //kitapbilgi.xml dosyasından veri okur.
+            string dosyaadi = "kitapbilgi.xml";
+            if (File.Exists(dosyaadi))//Eğer dosya varsa oku
+            {
+                XElement xmldosya2 = XElement.Load(dosyaadi);//İnternet adresi olabilir                                                                 //Belirli bir element'i elde etme
+                textBox1.Text = "";
+                IEnumerable<XElement> altdugumler = xmldosya2.Elements();
+                //Listeleme
+                foreach (var eleman in altdugumler)
+                {
+                    textBox1.Text += string.Format("Kitap adı: {0}, Yayınevi:{1}, Yazarı: {2}, Fiyatı: {3}",
+                        eleman.Element("isim").Value,
+                        eleman.Element("yayinevi").Value,
+                        eleman.Element("yazari").Value,
+                        eleman.Element("fiyat").Value + "\r\n");
+                }
+            }
+            else
+            {
+                MessageBox.Show("kitapbilgi.xml dosyası bulunamadı");
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //Öğrencileri oku.
+            //ogrenciler.xml dosyasından veri okur.
+            string dosyaadi = "ogrenciler.xml";
+            textBox1.Text = "";
+            if (File.Exists(dosyaadi))//Eğer dosya varsa oku
+            {
+                XElement xmldosya2 = XElement.Load(dosyaadi);//İnternet adresi olabilir
+                IEnumerable<XElement> altdugumler = xmldosya2.Elements();
+                //Listeleyelim
+                foreach (var eleman in altdugumler)
+                {
+                    textBox1.Text += string.Format("Öğrencinin numarası: {0}, Adı Soyadı:{1}, Bölümü: {2}, Yaşı: {3}",
+                        eleman.Element("numara").Value,
+                        eleman.Element("adsoyad").Value,
+                        eleman.Element("bolum").Value,
+                        eleman.Element("yas").Value + "\r\n");
+                }
+            }
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //Yaşı 20-25 arasında olan öğrenciler
+            textBox1.Text = "";
+            // XML verisini yükleyelim
+            XElement xmldosya = XElement.Load("ogrenciler.xml"); // XML dosyasının yolu
+
+            // Yaşı 20-25 arasında olan öğrencileri sorgulamak
+            var sonuc = from ogrenci in xmldosya.Elements("ogrenci")
+                        let yas = (int)ogrenci.Element("yas") // Yaş bilgisini alıyoruz bir değişkene atıyoruz
+                        where yas >= 20 && yas <= 25 // Yaş aralığına göre filtreliyoruz
+                        select new
+                        {
+                            Numara = ogrenci.Element("numara").Value,
+                            AdSoyad = ogrenci.Element("adsoyad").Value,
+                            Bolum = ogrenci.Element("bolum").Value,
+                            Yas = yas
+                        };
+            int sayac = 0;
+            // Sonuçları ekrana yazdırıyoruz
+            foreach (var ogrenci in sonuc)
+            {
+                textBox1.Text += ($"Numara: {ogrenci.Numara}, Ad Soyad: {ogrenci.AdSoyad}, Bölüm: {ogrenci.Bolum}, Yaş: {ogrenci.Yas}" + "\r\n");
+                sayac ++;
+            }
+            textBox1.Text += "Toplam:" + sayac;
+        }
+    }
+}
